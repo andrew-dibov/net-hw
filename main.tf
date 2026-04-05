2c216f04d61ad3ecf4566ac9d7a3d064ad3af623
Andrew Dibov <andrew.dibov@ya.ru>
2026-04-05 12:25:35 +1000

    helllo cat

--- main.tf
+++ main.tf
@@ -169,3 +169,132 @@ resource "yandex_compute_instance" "private_test" {
     ssh-keys = "ubuntu:${file(var.public_ssh_key_path)}"
   }
 }
+
+resource "yandex_storage_bucket" "storage_bucket" {
+  bucket        = "netology-storage-bucket"
+  force_destroy = true
+
+  anonymous_access_flags {
+    read        = true
+    list        = false
+    config_read = false
+  }
+}
+
+resource "yandex_storage_object" "picture" {
+  bucket       = yandex_storage_bucket.storage_bucket.bucket
+  key          = "image.jpg"
+  source       = "./image.jpg"
+  content_type = "image/jpeg"
+}
+
+resource "yandex_iam_service_account" "service_account_instance_group_web" {
+  name = "netology-iam-service-account-instance-group-web"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "resource_manager_service_account_instance_group_web" {
+  folder_id = var.yc_folder_id
+  role      = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.service_account_instance_group_web.id}"
+}
+
+resource "yandex_compute_instance_group" "compute_instance_group_web" {
+  name                = "netology-compute-instance-group-web"
+  folder_id           = var.yc_folder_id
+  deletion_protection = false
+  service_account_id  = yandex_iam_service_account.service_account_instance_group_web.id
+
+  load_balancer {
+    target_group_name = "netology-web-target-group"
+  }
+
+  instance_template {
+    platform_id = "standard-v3"
+    resources {
+      cores  = 2
+      memory = 2
+    }
+    boot_disk {
+      initialize_params {
+        image_id = "fd827b91d99psvq5fjit"
+        size     = 10
+      }
+    }
+    network_interface {
+      network_id = yandex_vpc_network.network.id
+      subnet_ids = [yandex_vpc_subnet.subnet_public.id]
+      nat        = true
+    }
+
+    metadata = {
+      ssh-keys  = "ubuntu:${file(var.public_ssh_key_path)}",
+      user-data = <<-EOF
+      #!/bin/bash
+      cat > /var/www/html/index.html << 'END'
+      <html>
+      <body>
+        <h1>Hello Cat</h1>
+        <img src="https://${yandex_storage_bucket.storage_bucket.bucket_domain_name}/${yandex_storage_object.picture.key}">
+      </body>
+      </html>
+      END
+      systemctl restart apache2
+      EOF
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 3
+    }
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 1
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
+  }
+
+  health_check {
+    interval = 30
+    timeout  = 10
+    tcp_options {
+      port = 80
+    }
+  }
+
+  depends_on = [yandex_storage_bucket.storage_bucket]
+}
+
+resource "yandex_lb_network_load_balancer" "load_balancer_web" {
+  name = "netology-load-balancer-web"
+
+  listener {
+    name = "http-listener"
+    port = 80
+
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.compute_instance_group_web.load_balancer[0].target_group_id
+
+    healthcheck {
+      name                = "http-health-check"
+      interval            = 30
+      timeout             = 10
+      unhealthy_threshold = 3
+      healthy_threshold   = 3
+
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
